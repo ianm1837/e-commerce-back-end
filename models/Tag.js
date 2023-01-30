e7cb44e2b6a65c168074b760b382c100c86f717c
ianm1837 <ian@iwm.dev>
2023-01-30 14:00:27 -0800

    added starter code; started adding models

---
+++ models/Tag.js
@@ -0,0 +1,27 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection.js');
+
+class Tag extends Model {}
+
+Tag.init(
+  {
+    //TODO: define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'tag',
+  }
+);
+
+module.exports = Tag;
